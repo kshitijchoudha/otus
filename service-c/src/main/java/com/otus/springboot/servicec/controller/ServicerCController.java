2f291aa3485d085cc3f898486e21e132a60edaa5
Madhu Chapati <msreddy_ch@hotmail.com>
2017-08-19 09:32:08 -0500

    adding 2 more services - C and D

---
+++ service-c/src/main/java/com/otus/springboot/servicec/controller/ServicerCController.java
@@ -0,0 +1,15 @@
+package com.otus.springboot.servicec.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServicerCController {
+	
+	@RequestMapping("/greeting")
+    public String greeting() {
+        return "OTUS";
+    }
+
+}
---
+++ service-d/src/main/java/com/otus/springboot/serviced/controller/ServicerDController.java
@@ -0,0 +1,15 @@
+package com.otus.springboot.serviced.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServicerDController {
+	
+	@RequestMapping("/greeting")
+    public String greeting() {
+        return "Madhu";
+    }
+
+}
