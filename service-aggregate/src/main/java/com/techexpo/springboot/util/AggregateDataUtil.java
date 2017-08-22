a443d1f1a9afe0075f38c0110fb622ffb21dae66
Madhu Chapati <msreddy_ch@hotmail.com>
2017-08-22 16:57:48 -0500

    Compilation error

--- service-aggregate/src/main/java/com/techexpo/springboot/util/AggregateDataUtil.java
+++ service-aggregate/src/main/java/com/techexpo/springboot/util/AggregateDataUtil.java
@@ -6,8 +6,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.techexpo.springboot.model.Instance;
 import com.techexpo.springboot.model.ServiceDetails;
 import com.techexpo.springboot.model.VPCFlowLogResponse;
