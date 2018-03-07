da00cb75ef0e18eb88dda7e3ac39170f06aa16fa
KC <kshitij.choudha@gmail.com>
2018-03-07 15:15:24 -0600

    Update fabfile.py

--- aws/fabfile.py
+++ aws/fabfile.py
@@ -2,6 +2,7 @@ from fabric.api import local, settings, abort, run, cd, sudo
 from fabric.contrib.console import confirm
 import datetime
 
+
 def test():
 	jarName="service-a-1.0-SNAPSHOT.jar"
 	serviceName=jarName[:-17]
