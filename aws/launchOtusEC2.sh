060517fafa63d2c1998ed348b1cb2f37b0f99d05
kshitijchoudha <kshitij.choudha@gmail.com>
2018-04-03 22:00:37 -0500

    jdk8

--- aws/launchOtusEC2.sh
+++ aws/launchOtusEC2.sh
@@ -10,6 +10,8 @@ for SERVICENAME in "${MICROSERVICE[@]}"
 do
 
 USERDATA=$"#!/bin/bash 
+sudo yum install java-1.8.0
+sudo yum remove java-1.7.0-openjdk
 cd /tmp  
 touch user-data-working   
 wget https://s3.us-east-2.amazonaws.com/otus-201804/lib/$SERVICENAME/$SERVICENAME-1.0-SNAPSHOT.jar 
