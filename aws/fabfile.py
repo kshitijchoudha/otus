591012c45b4468c3087c18fc1ea5792c3c4c53d7
kshitijchoudha <kshitij.choudha@gmail.com>
2018-04-14 08:12:00 -0500

    [ci skip]

--- aws/fabfile.py
+++ aws/fabfile.py
@@ -1,6 +1,29 @@
-from fabric.api import local, settings, abort, run, cd, sudo
+#Run random commands on ec2
+#fab run_command:"ps -ef|grep java" -H <public ip of ec2>
+
+#Deploy new jar on ec2 from s3 and restart boot app
+#fab deploy -H <public ip of ec2>  
+
+#Define roledefs and then call like this
+#fab run_command:"ps -ef|grep java" -R service-a
+
+#nohup java -jar service-aggregate-1.0-SNAPSHOT.jar AKIAJN25IADTAMDEDLIA A1Jb/vD5OCBIGH2bRi9e3O6/6XU8E634iauw3VXY true &
+
+#
+
+from fabric.api import local, settings, abort, run, cd, sudo, task, env
 from fabric.contrib.console import confirm
 import datetime
+
+
+env.user = 'ec2-user'
+env.key_filename = '/home/kchoudha/work/git/otus.pem'
+env.roledefs = {
+    'service-a': ['18.219.88.12'],
+    'aggr': ['18.188.102.66'],
+    'discovery': ['18.220.162.231'],
+    'ui' : ['18.188.172.233']
+}
 
 def test():
 	jarName="service-a-1.0-SNAPSHOT.jar"
@@ -22,3 +45,28 @@ def deploy():
         sudo("mv "+jarName+" "+jarName+"."+datetime.datetime.now().isoformat())
         run("wget https://s3.us-east-2.amazonaws.com/otus-201804/lib/"+serviceName+"/"+jarName)
     run("(nohup java -jar /tmp/"+jarName+"  > /tmp/nohup.out < /dev/null &)&", pty=False)
+
+def restart():
+    code_dir = '/tmp'
+    with cd(code_dir):
+        jarName=run("ls *.jar")
+        serviceName=jarName[:-17]
+        print 'jarName: '+jarName
+        print 'serviceName: '+serviceName
+        sudo("kill -9 `pgrep java` || true")
+    run("(nohup java -jar /tmp/"+jarName+"  > /tmp/nohup.out < /dev/null &)&", pty=False)
+
+def deploy_aggr():
+    code_dir = '/tmp'
+    with cd(code_dir):
+        jarName=run("ls *.jar")
+        serviceName=jarName[:-17]
+        print 'jarName: '+jarName
+        print 'serviceName: '+serviceName
+        sudo("kill -9 `pgrep java` || true")
+        sudo("mv "+jarName+" "+jarName+"."+datetime.datetime.now().isoformat())
+        run("wget https://s3.us-east-2.amazonaws.com/otus-201804/lib/"+serviceName+"/"+jarName)
+    run("(nohup java -jar /tmp/"+jarName+" <s3 read access key> <secret key> true > /tmp/nohup.out < /dev/null &)&", pty=False)
+
+def run_command(cmd):
+    run(cmd)
--- aws/launchOtusEC2.sh
+++ aws/launchOtusEC2.sh
@@ -10,8 +10,8 @@ for SERVICENAME in "${MICROSERVICE[@]}"
 do
 
 USERDATA=$"#!/bin/bash 
-sudo yum install java-1.8.0
-sudo yum remove java-1.7.0-openjdk
+sudo yum install java-1.8.0 -y
+sudo yum remove java-1.7.0-openjdk -y
 cd /tmp  
 touch user-data-working   
 wget https://s3.us-east-2.amazonaws.com/otus-201804/lib/$SERVICENAME/$SERVICENAME-1.0-SNAPSHOT.jar 
@@ -22,8 +22,8 @@ AWSCOMMAND="aws ec2 run-instances --image-id  ami-25615740 --count 1 --instance-
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$SERVICENAME},{Key=Project,Value=otus}]' \
 --user-data \"$USERDATA\""
 
-echo $SERVICENAME
-echo "$USERDATA"
+#echo $SERVICENAME
+#echo "$USERDATA"
 echo "$AWSCOMMAND"
 
 eval "$AWSCOMMAND"
@@ -34,6 +34,8 @@ for SERVICENAME in "${NANOSERVICE[@]}"
 do
 
 USERDATA=$"#!/bin/bash 
+sudo yum install java-1.8.0 -y
+sudo yum remove java-1.7.0-openjdk -y
 cd /tmp  
 touch user-data-working  
 wget https://s3.us-east-2.amazonaws.com/otus-201804/lib/$SERVICENAME/$SERVICENAME-1.0-SNAPSHOT.jar   
@@ -44,10 +46,17 @@ AWSCOMMAND="aws ec2 run-instances --image-id  ami-25615740 --count 1 --instance-
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$SERVICENAME},{Key=Project,Value=otus}]' \
 --user-data \"$USERDATA\""
 
-echo $SERVICENAME
-echo "$USERDATA"
+#echo $SERVICENAME
+#echo "$USERDATA"
 echo "$AWSCOMMAND"
 
 eval "$AWSCOMMAND"
 
 done
+
+#aws ec2 associate-address --instance-id `aws ec2 describe-instances  --output text --filters "Name=tag:Name,Values=service-discovery" "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].InstanceId'` --public-ip 18.220.162.231
+
+
+#aws ec2 associate-address --instance-id `aws ec2 describe-instances  --output text --filters "Name=tag:Name,Values=otus-ui" "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].InstanceId'` --public-ip 18.188.102.66
+
+
